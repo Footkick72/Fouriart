69f81e96163e77445b131122985a8cbf49c6a46b
Daniel Long <zhozholong@gmail.com>
2021-09-12 14:04:22 -0400

    Initial Commit

---
+++ Fouriart/FouriartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FouriartApp.swift
+//  Fouriart
+//
+//  Created by Daniel Long on 9/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FouriartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
